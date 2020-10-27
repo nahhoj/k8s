9913b59a98f51d171ebd0afd64afcebcd75bafae
Johan Calderon <nahhoj@hotmail.com>
2020-10-27 16:57:11 -0500

    add dockerFiles and docker compose

--- go/index.go
+++ go/index.go
@@ -13,9 +13,9 @@ func main() {
 		res.Header().Set("Content-Type", "text/html; charset=UTF-8")
 		res.WriteHeader(http.StatusOK)
 		fmt.Fprintf(res, "<h2>Hello World GO with kubernetes and microservice</h3>")
-		fmt.Fprintf(res, "<h3>Platform:"+runtime.GOOS+"</h3>")
+		fmt.Fprintf(res, "<h3>Platform: "+runtime.GOOS+"</h3>")
 		hostName, _ := os.Hostname()
-		fmt.Fprintf(res, "<h3>Hostmame:"+hostName+"</h3>")
+		fmt.Fprintf(res, "<h3>Hostmame: "+hostName+"</h3>")
 		var ip net.IP
 		iaddrs, _ := net.InterfaceAddrs()
 		for _, addr := range iaddrs {
@@ -24,7 +24,7 @@ func main() {
 				ip = a.IP
 			}
 		}
-		fmt.Fprintf(res, "<h3>IP:"+ip.String()+"</h3>")
+		fmt.Fprintf(res, "<h3>IP: "+ip.String()+"</h3>")
 	})
 	error := http.ListenAndServe(":3000", nil)
 	if error != nil {
